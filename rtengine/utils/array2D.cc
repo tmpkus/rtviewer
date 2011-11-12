d78be0fceb247e66ab22c5bf66efb469e8a5150f
janrinze <janrinze@localhost>
2011-11-12 09:41:18 +0100

    Added some comments

--- rtengine/utils/array2D.cc
+++ rtengine/utils/array2D.cc
@@ -5,3 +5,9 @@
  *      Author: janrinze
  */
 
+/*
+ * currently there is no code here since the code resides in the header file.
+ * By using the header file we can assure the best compilation result
+ * for both speed and the ability to have many different formats combined.
+ *
+ */
---
+++ rtengine/utils/logfile.h
@@ -0,0 +1,18 @@
+/*
+ * logfile.h
+ *
+ * currently a placeholder for logging messages. The implementation for now ignores
+ * all messages.
+ *
+ *  Created on: Jun 11, 2011
+ *      Author: janrinze
+ */
+
+#ifndef LOGFILE_H_
+#define LOGFILE_H_
+
+#define logmsg( ... ) { }
+#define error( ... ) { }
+#define logerr( ... ) { }
+
+#endif /* LOGFILE_H_ */
